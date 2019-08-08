f4dbe2e23f8c5742755bc9546257d8f2042aa4ef
nayeem833 <mdnayeemullah833@gmail.com>
2019-08-08 09:04:20 +0530

    First Python File Push

---
+++ patter.py
@@ -0,0 +1,19 @@
+# pattern
+#num=int(input("enter the number of rows"))
+#for i in range(1,num+1):
+	#for j in range(1,i+1):
+		#print("*",end=" ")
+	#print()	
+
+# Multiple time 9
+#num=9
+#for i in range(1,11):
+	#print(num,'x',i,'=',num*i)
+ 
+#range between to 1 in 50 divid by eithor 2 or 3
+#lower=int(input("Enter lower range limit:"))
+#upper=int(input("Enter upper range limit:"))	
+#n=int(input("Enter the number to be divide by:"))
+#for i in range(lower,upper+1):
+	#if(i%n==0):
+		#print(i)
